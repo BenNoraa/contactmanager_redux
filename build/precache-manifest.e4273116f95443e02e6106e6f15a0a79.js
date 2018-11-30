07cc631031410e27d34bdf49fd5ee62aaa07b048
noraa <bendicksonaaron@gmail.com>
2018-11-29 22:55:16 -0500

    first commit

---
+++ build/precache-manifest.e4273116f95443e02e6106e6f15a0a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "419279a280d5f84d3508",
+    "url": "/static/js/main.419279a2.chunk.js"
+  },
+  {
+    "revision": "00570977b8c73ea36831",
+    "url": "/static/js/1.00570977.chunk.js"
+  },
+  {
+    "revision": "419279a280d5f84d3508",
+    "url": "/static/css/main.a41f0652.chunk.css"
+  },
+  {
+    "revision": "00570977b8c73ea36831",
+    "url": "/static/css/1.319a1807.chunk.css"
+  },
+  {
+    "revision": "c12cb71c7f5f5c54f2eb360b6ebfe5df",
+    "url": "/index.html"
+  }
+];
